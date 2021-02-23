aac69bb030370a41f051fcc98e88c2994d32fe84
Esteban <acevedoesteban999@gmail.com>
2021-02-22 23:03:14 -0500

    v-1.0

---
+++ index.js
@@ -0,0 +1,16 @@
+const {app,BrowserWindow}=require('electron');
+const url=require('url');
+const path=require('path');
+let MW=null;
+app.on('ready',()=>{
+   MW=new BrowserWindow({});
+   MW.loadURL(url.format({
+        pathname:path.join(__dirname,'view/index.html'),
+        protocol:'file',
+        slashes:true
+
+   }));
+   
+
+})
+
